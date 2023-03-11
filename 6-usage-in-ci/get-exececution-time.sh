7b28b6caa2705c614a3cd01e31992efcbacc16f5
Josef Reinhard Brandl <mail@josefbrandl.de>
2023-03-11 21:25:07 +0100

    test

--- 6-usage-in-ci/get-exececution-time.sh
+++ 6-usage-in-ci/get-exececution-time.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Retrieve job data from Github API
-jobs=$(gh api -X GET /repos/MajorBreakfast/local-kubernetes-comparison/actions/runs/4393653965/jobs)
+jobs=$(gh api -X GET /repos/MajorBreakfast/local-kubernetes-comparison/actions/runs/4393894177/jobs --paginate)
 
 echo $jobs | jq '
   .jobs
@@ -9,3 +9,10 @@ echo $jobs | jq '
   | group_by(.job)
   | map( [{key: "job", value: .[0].job }] + map({key: .run, value: .duration}) )
 ' | jq -r '(.[0] | map(.key)), (.[] | map(.value)) | @csv' > 6-usage-in-ci/timings.csv
+
+echo $jobs | jq '
+  .jobs
+  | map({ run: (.name | split(" / ") | .[0] ), job: (.name | split(" / ") | .[1] ), duration: ((.completed_at | fromdate) - (.started_at | fromdate)) })
+  | group_by(.job)
+  | map( [{key: "job", value: .[0].job }, {key: "average", value: (map(.duration) | add / length) }])
+' | jq -r '(.[0] | map(.key)), (.[] | map(.value)) | @csv' > 6-usage-in-ci/timings-average.csv
